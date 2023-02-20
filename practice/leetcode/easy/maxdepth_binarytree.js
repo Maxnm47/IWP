d7226c735d68362e1022ce98db88366def71662e
Maxnm47 <96955526+Maxnm47@users.noreply.github.com>
2023-02-20 09:33:57 +0100

    yes

--- practice/leetcode/easy/maxdepth_binarytree.js
+++ practice/leetcode/easy/maxdepth_binarytree.js
@@ -1,16 +1,19 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {TreeNode} root
- * @return {number}
- */
 var maxDepth = function(root) {
-    
-}
+  if (!root) {
+    return 0;
+  }
+  
+  const leftDepth = maxDepth(root.left);
+  const rightDepth = maxDepth(root.right);
+  
+  return Math.max(leftDepth, rightDepth) + 1;
+};
 
+//faster
+var maxDepth = function(root) {
+    if(root==null)
+        return 0;
+    
+    return 1 + Math.max(maxDepth(root.left), maxDepth(root.right))
+
+};
